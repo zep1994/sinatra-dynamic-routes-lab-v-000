b0d4e538e84371d9635af1bb73996984eaae7431
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-08-25 18:16:08 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -32,6 +32,8 @@ class App < Sinatra::Base
     @operation = params[:operation]
     @number1 = params[:number1].to_i
     @number2 = params[:number2].to_i
-    
+      case :operation
+      when "add"
+        
   end
 end
