f4c7156f5fc6f906439b8b37c42eeabc62ffe292
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-08-25 18:14:15 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -27,4 +27,8 @@ class App < Sinatra::Base
     @word5 = params[:word5]
     "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
   end
+  
+  get '/:operation/:number1/:number2' do
+    
+  end
 end
