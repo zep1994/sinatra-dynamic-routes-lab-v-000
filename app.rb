12b64fc8436195c553a600e8bc5ae775dd1847f5
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-08-25 18:29:50 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -37,7 +37,7 @@ class App < Sinatra::Base
         "#{@number1 + @number2}"
       when "subtract"
         "#{@number1 - @number2}"
-      when "multply"
+      when "multiply"
         "#{@number1 * @number2}"
       when "divide"
         "#{@number1 / @number2}"
